9fc448ee89aeb85ca3ee5a6442ff6cc4d60bbe27
Jason Kai <tkai@uwo.ca>
2023-06-20 12:06:50 -0400

    gen affine xfm instead of reading from nii

--- afids_utils/tests/strategies.py
+++ afids_utils/tests/strategies.py
@@ -13,7 +13,7 @@ def afid_coords(
     max_value: float = 50.0,
     width: int = 16,
 ) -> NDArray[np.single]:
-    coords = draw(
+    return draw(
         arrays(
             shape=(32, 3),
             dtype=np.single,
@@ -23,16 +23,42 @@ def afid_coords(
         )
     )
 
-    return coords
 
-
+@st.composite
 def world_coord(
-    min_value: float = -50.0, max_value: float = 50.0, width: int = 16
+    draw: st.DrawFn, 
+    min_value: float = -50.0, 
+    max_value: float = 50.0, 
+    width: int = 16,
 ) -> NDArray[np.single]:
-    return arrays(
-        shape=(3, 1),
-        dtype=np.single,
-        elements=st.floats(
-            min_value=min_value, max_value=max_value, width=width
-        ),
+    return draw(
+        arrays(
+            shape=(3, 1),
+            dtype=np.single,
+            elements=st.floats(
+                min_value=min_value, max_value=max_value, width=width
+            ),
+        )
     )
+
+
+@st.composite
+def affine_xfm(
+    draw: st.DrawFn,
+    min_value: float = 1.0,
+    max_value: float= 150.0,
+    width: int = 16,
+) -> NDArray[np.single]:
+    
+    affine = np.eye(4)
+    # affine[:3, :3] = draw(
+    #     arrays(
+    #         shape=(3, 3),
+    #         dtype=np.single,
+    #         elements=st.floats(
+    #             min_value=min_value, max_value=max_value, width=width
+    #         )
+    #     )
+    # )
+
+    return affine
--- afids_utils/tests/test_transforms.py
+++ afids_utils/tests/test_transforms.py
@@ -4,35 +4,25 @@ from os import PathLike
 from pathlib import Path
 
 import numpy as np
+import nibabel as nib
 import pytest
 from hypothesis import HealthCheck, given, settings
 from hypothesis import strategies as st
 from numpy.typing import NDArray
 
-from afids_utils.io import load_nii
-from afids_utils.tests.strategies import world_coord
+from afids_utils.tests.strategies import world_coord, affine_xfm
 from afids_utils.transforms import afid_world2voxel
 
 
-@pytest.fixture
-def nii_file() -> PathLike[str]:
-    return (
-        Path(__file__).parent
-        / "data"
-        / "tpl-MNI152NLin2009cAsym_res-01_desc-brain_T1w.nii.gz"
-    )
-
-
 class TestAfidWorld2Voxel:
-    @given(world_coord=world_coord())
+    @given(world_coord=world_coord(), nii_affine=affine_xfm())
     @settings(
         deadline=400,
         suppress_health_check=[HealthCheck.function_scoped_fixture],
     )
     def test_transform_output(
-        self, world_coord: NDArray[np.single], nii_file: PathLike[str]
+        self, world_coord: NDArray[np.single], nii_affine: NDArray[np.single]
     ):
-        nii_affine, _ = load_nii(nii_file)
         voxel_coord = afid_world2voxel(world_coord, nii_affine)
 
         assert isinstance(voxel_coord, np.ndarray)
@@ -40,6 +30,7 @@ class TestAfidWorld2Voxel:
 
     @given(
         world_coord=world_coord(),
+        nii_affine=affine_xfm(),
         resample_size=st.floats(min_value=0.0, max_value=4.0),
     )
     @settings(
@@ -49,10 +40,9 @@ class TestAfidWorld2Voxel:
     def test_resample_transform(
         self,
         world_coord: NDArray[np.single],
-        nii_file: PathLike[str],
+        nii_affine: NDArray[np.single],
         resample_size: float,
     ):
-        nii_affine, _ = load_nii(nii_file)
         voxel_coord = afid_world2voxel(
             world_coord,
             nii_affine,
@@ -64,6 +54,7 @@ class TestAfidWorld2Voxel:
 
     @given(
         world_coord=world_coord(),
+        nii_affine=affine_xfm(),
         padding=st.integers(min_value=1, max_value=10),
     )
     @settings(
@@ -72,11 +63,10 @@ class TestAfidWorld2Voxel:
     )
     def test_padding_transform(
         self,
-        world_coord: NDArray[np.float_],
-        nii_file: PathLike[str],
+        world_coord: NDArray[np.single],
+        nii_affine: NDArray[np.single],
         padding: int,
     ):
-        nii_affine, _ = load_nii(nii_file)
         voxel_coord = afid_world2voxel(
             world_coord,
             nii_affine,

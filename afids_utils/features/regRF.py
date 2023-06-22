465900b902f44fcce7d251c5f26d717f80f914c2
Jason Kai <tkai@uwo.ca>
2023-06-22 11:47:58 -0400

    add coord region sampling and assoc. tests

--- afids_utils/features/regRF.py
+++ afids_utils/features/regRF.py
@@ -1,9 +1,11 @@
 """Core methods / functions for regRF"""
 from __future__ import annotations
 
+import itertools as it
 from typing import overload
 
 import numpy as np
+import polars as pl
 from numpy.typing import NDArray
 
 
@@ -26,3 +28,33 @@ def integral_volume(nii_img: NDArray[np.int_ | np.float_]):
     iv_zeropad[1:, 1:, 1:] = iv_img
 
     return iv_zeropad
+
+
+def sample_coord_region(
+    coord: NDArray[np.int_], sampling_rate: int, multiplier: int = 1
+) -> pl.DataFrame:
+    return pl.DataFrame(
+        list(
+            it.product(
+                *[
+                    range(
+                        coord[0] - sampling_rate * multiplier,
+                        coord[0] + sampling_rate * multiplier + 1,
+                        multiplier,
+                    ),
+                    range(
+                        coord[1] - sampling_rate * multiplier,
+                        coord[1] + sampling_rate * multiplier + 1,
+                        multiplier,
+                    ),
+                    range(
+                        coord[2] - sampling_rate * multiplier,
+                        coord[2] + sampling_rate * multiplier + 1,
+                        multiplier,
+                    ),
+                ]
+            )
+        ),
+        orient="row",
+        schema=["x", "y", "z"],
+    )
--- afids_utils/tests/test_regRF.py
+++ afids_utils/tests/test_regRF.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
 import numpy as np
+import polars as pl
 from hypothesis import given
+from hypothesis import strategies as st
+from hypothesis.extra.numpy import arrays
 from numpy.typing import NDArray
 
 from afids_utils.features import regRF
@@ -26,3 +29,59 @@ class TestIntegralVolume:
             assert iv_img.dtype == np.float_
         else:
             assert iv_img.dtype == np.int_
+
+
+class TestSampleCoordRegion:
+    @given(
+        coord=arrays(
+            shape=(3),
+            dtype=np.int_,
+            elements=st.integers(min_value=0, max_value=16),
+        ),
+        sampling_rate=st.integers(min_value=2, max_value=10),
+        multiplier=st.integers(min_value=1, max_value=4),
+    )
+    def test_sample_coord_region(
+        self,
+        coord: NDArray[np.int_],
+        sampling_rate: int,
+        multiplier: int,
+    ):
+        sampled_region = regRF.sample_coord_region(
+            coord=coord, sampling_rate=sampling_rate, multiplier=multiplier
+        )
+
+        # Check correct return type
+        assert isinstance(sampled_region, pl.DataFrame)
+
+        # Check outputs correctly generated
+        assert np.all(
+            (
+                np.unique(sampled_region["x"].to_numpy())
+                == range(
+                    coord[0] - sampling_rate * multiplier,
+                    coord[0] + sampling_rate * multiplier + 1,
+                    multiplier,
+                )
+            )
+        )
+        assert np.all(
+            (
+                np.unique(sampled_region["y"].to_numpy())
+                == range(
+                    coord[1] - sampling_rate * multiplier,
+                    coord[1] + sampling_rate * multiplier + 1,
+                    multiplier,
+                )
+            )
+        )
+        assert np.all(
+            (
+                np.unique(sampled_region["z"].to_numpy())
+                == range(
+                    coord[2] - sampling_rate * multiplier,
+                    coord[2] + sampling_rate * multiplier + 1,
+                    multiplier,
+                )
+            )
+        )

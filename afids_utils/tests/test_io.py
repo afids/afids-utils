4cce201f54889389e04d71640469e8ed161004d8
Jason Kai <tkai@uwo.ca>
2023-08-02 14:26:32 -0400

    fix tests with contextmanager

--- afids_utils/tests/test_io.py
+++ afids_utils/tests/test_io.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import csv
 import json
 import tempfile
-from os import PathLike
+from os import PathLike, remove
 from pathlib import Path
 
 import numpy as np
@@ -73,7 +73,7 @@ class TestGetAfidFcsv:
         desc=st.text(
             min_size=2,
             max_size=5,
-            alphabet=st.characters(blacklist_categories=("P", "Nd")),
+            alphabet=st.characters(whitelist_categories=("L")),
         ),
     )
     @settings(
@@ -87,32 +87,29 @@ class TestGetAfidFcsv:
         # Replace valid description with a mismatch
         with open(valid_fcsv_file, "r", encoding="utf-8") as f:
             content = f.read()
-
         content = content.replace(EXPECTED_DESCS[afid_num - 1][0], desc)
 
         # Write to temp file
-        out_fcsv_file = tempfile.NamedTemporaryFile(
+        with tempfile.NamedTemporaryFile(
             mode="w",
-            delete=False,
             prefix="sub-test_desc-",
             suffix="_afids.fcsv",
-        )
-        with open(out_fcsv_file.name, "w") as f:
-            f.write(content)
+        ) as out_fcsv_file:
+            out_fcsv_file.write(content)
+            out_fcsv_file.flush()
 
-        # Test for validity
-        with pytest.raises(ValueError, match=".*does not match expected*."):
-            get_afid(out_fcsv_file.name, afid_num)
-
-        # Delete temp file
-        remove(out_fcsv_file.name)
+            # Test for validity
+            with pytest.raises(
+                ValueError, match=".*does not match expected*."
+            ):
+                get_afid(out_fcsv_file.name, afid_num)
 
     @given(
         afid_num=st.integers(min_value=1, max_value=32),
         ext=st.text(
             min_size=2,
             max_size=5,
-            alphabet=st.characters(blacklist_categories=("P", "Nd")),
+            alphabet=st.characters(whitelist_categories=("L")),
         ),
     )
     @settings(
@@ -142,7 +139,7 @@ class TestGetAfidJson:
         assert isinstance(afid, np.ndarray)
         # Check array values
         assert afid.dtype == np.single
-
+ 
     @given(afid_num=st.integers(min_value=-1000, max_value=1000))
     @settings(
         suppress_health_check=[HealthCheck.function_scoped_fixture],
@@ -153,18 +150,18 @@ class TestGetAfidJson:
         afid_num: int,
     ) -> None:
         assume(afid_num < 1 or afid_num > 32)
-
+ 
         with pytest.raises(
             InvalidFiducialNumberError, match="Invalid fiducial number*."
         ):
             get_afid(valid_json_file, afid_num)
-
+ 
     @given(
         afid_num=st.integers(min_value=1, max_value=32),
         desc=st.text(
             min_size=2,
             max_size=5,
-            alphabet=st.characters(blacklist_categories=("P", "Nd")),
+            alphabet=st.characters(whitelist_categories=("L")),
         ),
     )
     @settings(
@@ -181,30 +178,29 @@ class TestGetAfidJson:
         content["markups"][0]["controlPoints"][afid_num - 1][
             "description"
         ] = desc
-
+        
         # Write to temp file
-        out_json_file = tempfile.NamedTemporaryFile(
-            mode="w",
-            delete=False,
-            prefix="sub-test_desc-",
-            suffix="_afids.json",
-        )
-        with open(out_json_file.name, "w") as f:
-            json.dump(content, f, indent=4)
+        with tempfile.NamedTemporaryFile(
+            mode="w", delete=False, prefix="sub-test_desc-", suffix="_afids.json"
+        ) as out_json_file:
+            json.dump(content, out_json_file, indent=4)
+            out_file_path = out_json_file.name
 
         # Test for validity
-        with pytest.raises(ValueError, match=".*does not match expected*."):
-            get_afid(out_json_file.name, afid_num)
+        # Needs to be outside of context, otherwise JSONDecodeError thrown
+        with pytest.raises(
+            ValueError, match=".*does not match expected description"
+        ) as err:
+            get_afid(out_file_path, afid_num)
 
-        # Delete temp file
-        remove(out_json_file.name)
+        remove(out_file_path)
 
     @given(
         afid_num=st.integers(min_value=1, max_value=32),
         ext=st.text(
             min_size=2,
             max_size=5,
-            alphabet=st.characters(blacklist_categories=("P", "Nd")),
+            alphabet=st.characters(whitelist_categories=("L")),
         ),
     )
     @settings(
@@ -215,7 +211,7 @@ class TestGetAfidJson:
     ) -> None:
         assume(not ext == "fcsv" or not ext == "json")
         invalid_file_ext = valid_json_file.with_suffix(f".{ext}")
-
+ 
         with pytest.raises(IOError, match="Invalid file extension"):
             get_afid(invalid_file_ext, afid_num)
 
@@ -231,6 +227,75 @@ class TestAfidsToFcsv:
                 "/invalid/fcsv/path",
             )
 
+    @given(afids_coords=afid_coords())
+    @settings(
+        suppress_health_check=[HealthCheck.function_scoped_fixture],
+    )
+    def test_write_fcsv(
+        self, afids_coords: NDArray[np.single], valid_fcsv_file: PathLike[str]
+    ) -> None:
+        with tempfile.NamedTemporaryFile(
+            mode="w", prefix="sub-test_desc-", suffix="_afids.fcsv"
+        ) as out_fcsv_file:
+            # Create and check output file
+            afids_to_fcsv(afids_coords, out_fcsv_file.name)
+
+            # Load files
+            with open(
+                valid_fcsv_file, "r", encoding="utf-8", newline=""
+            ) as template_fcsv_file, open(
+                out_fcsv_file.name, "r", encoding="utf-8", newline=""
+            ) as output_fcsv_file:
+                template_header = [
+                    template_fcsv_file.readline() for _ in range(3)
+                ]
+                output_header = [output_fcsv_file.readline() for _ in range(3)]
+                reader = csv.DictReader(
+                    output_fcsv_file, fieldnames=FCSV_FIELDNAMES
+                )
+                output_fcsv = list(reader)
+
+            # Check header
+            assert output_header == template_header
+            # Check contents
+            for idx, row in enumerate(output_fcsv):
+                assert (row["x"], row["y"], row["z"]) == (
+                    str(afids_coords[idx][0]),
+                    str(afids_coords[idx][1]),
+                    str(afids_coords[idx][2]),
+                )
+
+
+class TestAfidsToJson:
+    @given(afids_coords=afid_coords())
+    def test_invalid_json_template(
+        self, afids_coords: NDArray[np.single]
+    ) -> None:
+        with pytest.raises(FileNotFoundError):
+            afids_to_json(
+                afids_coords,
+                "/invalid/json/path",
+            )
+
+    @given(afids_coords=afid_coords())
+    @settings(
+        suppress_health_check=[HealthCheck.function_scoped_fixture],
+    )
+    def test_write_json(
+        self, afids_coords: NDArray[np.single], valid_json_file: PathLike[str]
+    ) -> None:
+        with tempfile.NamedTemporaryFile(
+            mode="w",
+            prefix="sub-test_desc-",
+            suffix="_afids.json",
+        ) as out_json_file:
+            afids_to_json(afids_coords, out_json_file.name)
+
+            # Check file was created
+            assert Path(out_json_file.name).exists()
+
+
+class TestAfidsIOGeneral:
     @given(afids_coords=afid_coords(bad_range=True))
     def test_invalid_num_afids(self, afids_coords: NDArray[np.single]) -> None:
         with tempfile.NamedTemporaryFile(
@@ -252,86 +317,3 @@ class TestAfidsToFcsv:
                 afids_to_fcsv(afids_coords, out_fcsv_file)
 
             assert "Expected 3 spatial dimensions" in str(err.value)
-
-    @given(afids_coords=afid_coords())
-    @settings(
-        suppress_health_check=[HealthCheck.function_scoped_fixture],
-    )
-    def test_write_fcsv(
-        self, afids_coords: NDArray[np.single], valid_fcsv_file: PathLike[str]
-    ) -> None:
-        out_fcsv_file = tempfile.NamedTemporaryFile(
-            mode="w",
-            delete=False,
-            prefix="sub-test_desc-",
-            suffix="_afids.fcsv",
-        )
-        out_fcsv_path = Path(out_fcsv_file.name)
-
-        afids_to_fcsv(afids_coords, out_fcsv_path)
-
-        # Check file was created
-        assert out_fcsv_path.exists()
-
-        # Load files
-        with open(
-            valid_fcsv_file, "r", encoding="utf-8", newline=""
-        ) as template_fcsv_file:
-            template_header = [template_fcsv_file.readline() for _ in range(3)]
-
-        with open(
-            out_fcsv_path, "r", encoding="utf-8", newline=""
-        ) as output_fcsv_file:
-            output_header = [output_fcsv_file.readline() for _ in range(3)]
-            reader = csv.DictReader(
-                output_fcsv_file, fieldnames=FCSV_FIELDNAMES
-            )
-            output_fcsv = list(reader)
-
-        # Check header
-        assert output_header == template_header
-        # Check contents
-        for idx, row in enumerate(output_fcsv):
-            assert (row["x"], row["y"], row["z"]) == (
-                str(afids_coords[idx][0]),
-                str(afids_coords[idx][1]),
-                str(afids_coords[idx][2]),
-            )
-
-        # Delete temporary file
-        remove(out_fcsv_path)
-
-
-class TestAfidsToJson:
-    @given(afids_coords=afid_coords())
-    def test_invalid_json_template(
-        self, afids_coords: NDArray[np.single]
-    ) -> None:
-        with pytest.raises(FileNotFoundError):
-            afids_to_json(
-                afids_coords,
-                "/invalid/json/path",
-            )
-
-    @given(afids_coords=afid_coords())
-    @settings(
-        suppress_health_check=[HealthCheck.function_scoped_fixture],
-    )
-    def test_write_json(
-        self, afids_coords: NDArray[np.single], valid_json_file: PathLike[str]
-    ) -> None:
-        out_json_file = tempfile.NamedTemporaryFile(
-            mode="w",
-            delete=False,
-            prefix="sub-test_desc-",
-            suffix="_afids.json",
-        )
-        out_json_path = Path(out_json_file.name)
-
-        afids_to_json(afids_coords, out_json_path)
-
-        # Check file was created
-        assert out_json_path.exists()
-
-        # Delete temporary file
-        remove(out_json_path)
